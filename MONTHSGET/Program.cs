f22d4ef215dca6f9439442e8ecd2b2e61fc634ab
Amir <Amir@WIN-S7KG9HKT0QA>
2023-12-26 17:35:17 +0600

    Добавьте файлы проекта.

---
+++ MONTHSGET/Program.cs
@@ -0,0 +1,47 @@
+﻿using MONTHSGET;
+using System.Data.SqlClient;
+
+namespace monthsGet
+{
+    internal class Program
+    {
+        static void Main()
+        {
+            string connectionString = "Data Source=(localdb)\\MSSQLLocalDB;Initial Catalog=testdb;Integrated Security=True;Connect Timeout=30;Encrypt=False;TrustServerCertificate=False;ApplicationIntent=ReadWrite;MultiSubnetFailover=False"; // Замените на свою строку подключения
+
+
+            Console.WriteLine(GetMonthNameById(connectionString, 2)); 
+            Console.WriteLine(GetMonthNameById(connectionString, 11));
+            Console.WriteLine(GetMonthNameById(connectionString, 13));
+        }
+
+        static string GetMonthNameById(string connectionString, int monthId)
+        {
+            using (System.Data.SqlClient.SqlConnection connection = new System.Data.SqlClient.SqlConnection(connectionString))
+            {
+                connection.Open();
+
+                // Создание команды SQL
+                string sql = "SELECT name FROM Months WHERE id = @monthId";
+                using (SqlCommand command = new SqlCommand(sql, connection))
+                {
+                    // Добавление параметра
+                    command.Parameters.AddWithValue("@monthId", monthId);
+
+                    // Выполнение запроса
+                    object result = command.ExecuteScalar();
+
+                    // Проверка наличия результата
+                    if (result! != null)
+                    {
+                        return result.ToString();
+                    }
+                    else
+                    {
+                        return "";
+                    }
+                }
+            }
+        }
+    }
+}
---
+++ MONTHSGET/SqlConnection.cs
@@ -0,0 +1,12 @@
+﻿namespace MONTHSGET
+{
+    internal class SqlConnection
+    {
+        private string connectionString;
+
+        public SqlConnection(string connectionString)
+        {
+            this.connectionString = connectionString;
+        }
+    }
+}
